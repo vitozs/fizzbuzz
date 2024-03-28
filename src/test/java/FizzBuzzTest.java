7a987408e86329e73bc13df10cc79ec6a1f76509
Vitor <vitornuneschagas2016@gmail.com>
2024-03-28 00:44:50 -0300

    more unit tests

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -11,6 +11,50 @@ public class FizzBuzzTest {
     }
 
     @Test
+    void testAddStringIfDivisible(){
+        InputResponseBuilder inputResponseBuilder = new InputResponseBuilder(9);
+        inputResponseBuilder.addStringIfDivisible("Fizz", 3);
+        String result = inputResponseBuilder.getResult();
+
+        Assertions.assertEquals("Fizz", result);
+    }
+    @Test
+    void testEmptyStringIfNotDivisible(){
+        InputResponseBuilder inputResponseBuilder = new InputResponseBuilder(2);
+        inputResponseBuilder.addStringIfDivisible("Fizz", 3);
+        String result = inputResponseBuilder.getResult();
+
+        Assertions.assertEquals("", result);
+    }
+    @Test
+    void testAddStringIfContainsNumber(){
+        InputResponseBuilder inputResponseBuilder = new InputResponseBuilder(32);
+        inputResponseBuilder.addStringIfContainsNumber("Fizz", 3);
+        String result = inputResponseBuilder.getResult();
+
+        Assertions.assertEquals("Fizz", result);
+    }
+    @Test
+    void testEmptyStringIfNotContainsNumber(){
+        InputResponseBuilder inputResponseBuilder = new InputResponseBuilder(22);
+        inputResponseBuilder.addStringIfContainsNumber("Fizz", 3);
+        String result = inputResponseBuilder.getResult();
+
+        Assertions.assertEquals("", result);
+    }
+    @Test
+    void testUserInputHasTwoOrMoreDigits(){
+        InputResponseBuilder inputResponseBuilder = new InputResponseBuilder(10);
+
+        Assertions.assertTrue(inputResponseBuilder.userInputHasTwoDigits());
+    }
+    @Test
+    void testUserInputHasOneDigit(){
+        InputResponseBuilder inputResponseBuilder = new InputResponseBuilder(7);
+
+        Assertions.assertFalse(inputResponseBuilder.userInputHasTwoDigits());
+    }
+    @Test
     void testForMultipleOfThree(){
         Assertions.assertEquals("Fizz", fizzBuzz.getFizzBuzzOfNumber(9));
     }
